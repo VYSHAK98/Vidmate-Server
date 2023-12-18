fee049918c8c4b32f971bbb386c33d63e164eab9
vyshak harikumar <vyshakharikumar98@gmail.com>
2023-12-18 08:46:33 +0530

    vidmateServer

---
+++ index.js
@@ -0,0 +1,17 @@
+const jsonServer=require('json-server')
+const cors=require('cors')
+
+const server=jsonServer.create()
+// to convert json to js
+const middleware=jsonServer.defaults()
+const router=jsonServer.router('data.json')
+
+server.use(cors())
+server.use(middleware)
+server.use(router)
+
+const PORT=4000
+server.listen(PORT,()=>{
+    console.log(`....Json Server Started At Port ${PORT}....`);
+})
+
